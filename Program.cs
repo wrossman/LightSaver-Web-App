6fca1ec3b14cef1a1ae77700dfd1fde6f2a5fa2f
Bill Rossman <william.m.rossman@gmail.com>
2025-11-11 07:40:31 -0800

    refactor callback endpoint and googleflow

--- Program.cs
+++ Program.cs
@@ -38,7 +38,6 @@ app.MapGet("/test", async (IConfiguration config, HttpContext context, SessionDb
         return Results.Unauthorized();
     }
 
-
     string clientId = config["OAuth:ClientId"] ?? string.Empty;
     string redirect = config["OAuth:RedirectUri"] ?? string.Empty;
     string responseType = config["OAuth:ResponseType"] ?? string.Empty;
@@ -48,25 +47,15 @@ app.MapGet("/test", async (IConfiguration config, HttpContext context, SessionDb
     return Results.Content(googleQuery);
 });
 
-app.MapGet("/roku", (IConfiguration config) =>
+app.MapGet("/auth/google-callback", static async (HttpContext context, IServiceProvider serviceProvider) =>
 {
-    string clientId = config["OAuth:ClientId"] ?? string.Empty;
-    string redirect = config["OAuth:RedirectUri"] ?? string.Empty;
-    string responseType = config["OAuth:ResponseType"] ?? string.Empty;
-    string scope = config["OAuth:PickerScope"] ?? string.Empty;
-    string googleAuthServer = config["OAuth:GoogleAuthServer"] ?? string.Empty;
-    string googleQuery = $"{googleAuthServer}?scope={scope}&response_type={responseType}&redirect_uri={redirect}&client_id={clientId}";
-    return Results.Redirect(googleQuery);
-});
 
-app.MapGet("/auth/google-callback", async (HttpContext context, IServiceProvider serviceProvider) =>
-{
+    // ADD RATE LIMITING FOR ENDPOINTS
     var request = context.Request;
     var config = context.RequestServices.GetRequiredService<IConfiguration>();
 
     if (context.Request.Query.ContainsKey("error"))
         return Results.BadRequest($"Failed with error: {context.Request.Query["error"]}");
-
     var authCode = request.Query["code"];
     if (authCode == StringValues.Empty)
         return Results.BadRequest("Unable to get Authorization Code from Google");
@@ -74,27 +63,27 @@ app.MapGet("/auth/google-callback", async (HttpContext context, IServiceProvider
     if (authCodeString is null)
         return Results.BadRequest("Google OAuth Response Failed to provide Authorization String");
 
-    GoogleTokenResponse? accessTokenJson = await GoogleOAuth.GetAccessToken(context, config, authCodeString);
-    // ADD TRACKING FOR ACCESSTOKENS
-    if (accessTokenJson is null)
-        return Results.BadRequest("Failed to retrieve Access Token");
-    string accessToken = accessTokenJson.AccessToken;
-
-    (PickerSession?, PollingConfig?) pickerSession = ((PickerSession?, PollingConfig?))await GooglePhotos.GetPickerSession(context, config, accessToken);
-    // CREATE POLLING INSTANCE FOR PICKERSESSION
-    if (pickerSession.Item1 is null || pickerSession.Item2 is null)
-        return Results.BadRequest("Failed to retrieve Picker URI");
-    string pickerUri = pickerSession.Item1.PickerUri;
+    (PickerSession, PollingConfig) pickerSession = new();
     try
     {
-        GooglePhotos myGoogleFlow = new();
-        _ = myGoogleFlow.StartGooglePhotosFlow(pickerSession, accessToken, serviceProvider, config);
+        GoogleFlow googleFlow = new();
+        pickerSession = await googleFlow.GoogleAuthFlow(context, config, authCodeString, serviceProvider);
+    }
+    catch (Exception e) { Results.BadRequest(e.Message); }
+
+    string pickerUri = "";
+    if (pickerSession.Item1 is not null && pickerSession.Item2 is not null)
+        pickerUri = pickerSession.Item1.PickerUri;
+
+    try
+    {
+        GooglePhotosFlow photosFlow = new();
+        _ = photosFlow.StartGooglePhotosFlow(pickerSession, serviceProvider, config);
     }
     catch (Exception e)
     {
         System.Console.WriteLine($"Exited with error: {e.Message}");
     }
-
 
     return Results.Redirect($"{pickerUri}/autoclose");
 });
