aad870e6cb7908f5c62d3559984ecd3e0411d397
Bill Rossman <william.m.rossman@gmail.com>
2025-12-05 07:43:57 -0800

    fix upload page again, add session expiration check to each image service endpoint

--- Endpoints/GooglePhotosEndpoints.cs
+++ Endpoints/GooglePhotosEndpoints.cs
@@ -42,11 +42,17 @@ public static class GooglePhotosEndpoints
             return GlobalHelpers.CreateErrorPage("LightSaver requires cookies to be enabled to link your devices.", "Please enable Cookies and try again.");
         }
 
-        LinkSession? LinkSession = linkSessions.GetSession<LinkSession>(sessionId);
-        if (LinkSession is null)
+        LinkSession? linkSession = linkSessions.GetSession<LinkSession>(sessionId);
+        if (linkSession is null)
         {
             logger.LogWarning("Failed to get user session using session id at google handle oauth response endpoint");
             return GlobalHelpers.CreateErrorPage("There was a problem linking your google account to lightsaver.");
+        }
+
+        if (linkSession.Expired == true)
+        {
+            logger.LogWarning("User tried to upload photos with an expired session.");
+            return GlobalHelpers.CreateErrorPage("Your session has expired.", "<a href=\"/link/session\">Please Try Again</a>");
         }
 
         GoogleTokenResponse? accessTokenJson = await google.GetAccessToken(authCodeString);
