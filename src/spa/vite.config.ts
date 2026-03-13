c5df0cd868c20c4b893c2335947654e5ba50679b
Bill Rossman <william.m.rossman@gmail.com>
2026-03-12 19:22:11 -0700

    add spa build to azure app service deploymnet

--- src/spa/vite.config.ts
+++ src/spa/vite.config.ts
@@ -4,4 +4,8 @@ import basicSsl from "@vitejs/plugin-basic-ssl"
 
 export default defineConfig({
   plugins: [react(), basicSsl()],
+  build: {
+    outDir: "../api/wwwroot",
+    emptyOutDir: true
+  }
 })
